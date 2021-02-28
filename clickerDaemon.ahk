f1785f742c2cb1f01ea81e617b54ff94f1d6ea1d
Jack <jack.hudson12@yahoo.com>
2021-02-27 19:43:49 -0500

    Created end key functionality

--- clickerDaemon.ahk
+++ clickerDaemon.ahk
@@ -1,9 +1,18 @@
 #j:: 
+isRunning = 1 
 Loop { 
+if(isRunning = 1) { 
 MouseClick, left 
 Sleep 1000 
 } 
+if(isRunning = 0) { 
+break 
+} 
+} 
 return 
 #k:: 
+isRunning = 0 
+return 
+Escape:: 
 ExitApp 
 return 
--- daemonFormatter.bat
+++ daemonFormatter.bat
@@ -1,11 +1,19 @@
 @echo off
 echo %2:: > clickerDaemon.ahk
+echo isRunning = 1 >> clickerDaemon.ahk
 echo Loop { >> clickerDaemon.ahk
+echo if(isRunning = 1) { >> clickerDaemon.ahk
 echo MouseClick, %4 >> clickerDaemon.ahk
 echo Sleep %1 >> clickerDaemon.ahk
 echo } >> clickerDaemon.ahk
+echo if(isRunning = 0) { >> clickerDaemon.ahk
+echo break >> clickerDaemon.ahk
+echo } >> clickerDaemon.ahk
+echo } >> clickerDaemon.ahk
 echo return >> clickerDaemon.ahk
 echo %3:: >> clickerDaemon.ahk
+echo isRunning = 0 >> clickerDaemon.ahk
+echo return >> clickerDaemon.ahk
+echo Escape:: >> clickerDaemon.ahk
 echo ExitApp >> clickerDaemon.ahk
 echo return >> clickerDaemon.ahk
-clickerDaemon.ahk
